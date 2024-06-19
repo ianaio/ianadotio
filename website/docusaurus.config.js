fb66dd18a5019e1813392f0eb2db46af5c2a838c
Jaroslaw Cichon <dev@iana.io>
2024-06-19 17:50:46 +0000

    exmaples add

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,7 +13,7 @@ const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 const config = {
   title: 'IanaIO',
   tagline: ' A hybrid crypto data security software. Provides the best quality and a unique data not only for global community but also for governments and corporations. ',
-  url: 'https://www.iana.io',
+  url: 'https://security.iana.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
